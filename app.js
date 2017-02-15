714e6788d4d2ecd6e77bf8eea93f7b896097618f
徐民军 <mj@xuminjundeMacBook-Pro.local>
2017-02-15 14:17:54 +0800

    12313

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // token令牌
-app.use('/wechat',wechat(req, res, next));
+app.use('/wechat',wechat);
 // 前台
 app.use('/', index);
 // 后台
