3b862f333f0b15a70bbd6b8fe39cff5dea3532ce
徐民军 <mj@xuminjundeMacBook-Pro.local>
2017-02-15 14:05:28 +0800

    123

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,41 +1,46 @@
 var express = require('express');
 var router = express.Router();
-const config=require('../config/wechat.js');
-const crypto = require('crypto');
+const config = require('../config/wechat.js');
+var wechat = require('wechat');
 
-router.get('/', function(req, res, next) {
- 	wechatAuth(req, res);
+// router.get('/', function(req, res, next) {
+
+// });
+router.get('/', function(req, res.next) {
+  wechat(config, function(req, res.next) {
+    // 微信输入信息都在req.weixin上 
+    var message = req.weixin;
+    if (message.FromUserName === 'diaosi') {
+      // 回复屌丝(普通回复) 
+      res.reply('hehe');
+    } else if (message.FromUserName === 'text') {
+      //你也可以这样回复text类型的信息 
+      res.reply({
+        content: 'text object',
+        type: 'text'
+      });
+    } else if (message.FromUserName === 'hehe') {
+      // 回复一段音乐 
+      res.reply({
+        type: "music",
+        content: {
+          title: "来段音乐吧",
+          description: "一无所有",
+          musicUrl: "http://mp3.com/xx.mp3",
+          hqMusicUrl: "http://mp3.com/xx.mp3",
+          thumbMediaId: "thisThumbMediaId"
+        }
+      });
+    } else {
+      // 回复高富帅(图文回复) 
+      res.reply([{
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }]);
+    }
+  });
+
 });
 module.exports = router;
-
-// 功能性函数
-//进行sha1加密
-function sha1(str) {
-  var shasum = crypto.createHash("sha1");
-  shasum.update(str);
-  str = shasum.digest("hex");
-  return str;
-}
-// 微信信息验证
-function wechatAuth(req, res) {
-	console.log(res);
-  var query = url.parse(req.url, true).query;
-  var signature = query.signature;
-  var echostr = query.echostr;
-  var timestamp = query['timestamp'];
-  var nonce = query.nonce;
-
-  var reqArray = [nonce, timestamp, config.token];
-
-  //对数组进行字典排序
-  reqArray.sort();
-  var sortStr = reqArray.join('');//连接数组
-  var sha1Str = sha1(sortStr);
-
-  if (signature === sha1Str) {
-    res.end(echostr);
-  } else {
-    res.end("false");
-    console.log("授权失败!");
-  }
-}
