e66d131f6b00771703bc8910303b32dc9f30587c
徐民军 <mj@xuminjundeMacBook-Pro.local>
2017-02-15 13:36:52 +0800

    123

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var api = require('./routes/api');
-var token=require('./toutes/token');
+var token=require('./routes/token');
 var app = express();
 
 // view engine setup
