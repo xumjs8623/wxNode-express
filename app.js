c1e5b92f000a9eaad7e56d6b2acbae1b0751e94b
徐民军 <mj@xuminjundeMacBook-Pro.local>
2017-02-16 10:11:48 +0800

    123

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+// 引入路由js
 var index = require('./routes/index');
 var users = require('./routes/users');
 var api = require('./routes/api');
-var wechat=require('./routes/wechat1');
 var app = express();
-
+// 引入微信api集合和微信配置参数
 var wechat = require('wechat');
 var config=require('./config/wechat.js');
-
-
+// 引入微信关键词回复模块
+var wechatReply=require('./controller/wechat/reply.js');
+// 设置静态资源和模板引擎
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -40,38 +40,39 @@ app.use('/wechat', wechat(config, function (req, res, next) {
   // 微信输入信息都在req.weixin上
   var message = req.weixin;
   console.log(message);
-  if (message.FromUserName === 'diaosi') {
-    // 回复屌丝(普通回复)
-    res.reply('hehe');
-  } else if (message.FromUserName === 'text') {
-    //你也可以这样回复text类型的信息
-    res.reply({
-      content: 'text object',
-      type: 'text'
-    });
-  } else if (message.FromUserName === 'hehe') {
-    // 回复一段音乐
-    res.reply({
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3",
-        thumbMediaId: "thisThumbMediaId"
-      }
-    });
-  } else {
-    // 回复高富帅(图文回复)
-    res.reply([
-      {
-        title: '你来我家接我吧',
-        description: '这是女神与高富帅之间的对话',
-        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-        url: 'http://nodeapi.cloudfoundry.com/'
-      }
-    ]);
-  }
+  wechatReply(message,req);
+  // if (message.FromUserName === 'diaosi') {
+  //   // 回复屌丝(普通回复)
+  //   res.reply('hehe');
+  // } else if (message.FromUserName === 'text') {
+  //   //你也可以这样回复text类型的信息
+  //   res.reply({
+  //     content: 'text object',
+  //     type: 'text'
+  //   });
+  // } else if (message.FromUserName === 'hehe') {
+  //   // 回复一段音乐
+  //   res.reply({
+  //     type: "music",
+  //     content: {
+  //       title: "来段音乐吧",
+  //       description: "一无所有",
+  //       musicUrl: "http://mp3.com/xx.mp3",
+  //       hqMusicUrl: "http://mp3.com/xx.mp3",
+  //       thumbMediaId: "thisThumbMediaId"
+  //     }
+  //   });
+  // } else {
+  //   // 回复高富帅(图文回复)
+  //   res.reply([
+  //     {
+  //       title: '你来我家接我吧',
+  //       description: '这是女神与高富帅之间的对话',
+  //       picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+  //       url: 'http://nodeapi.cloudfoundry.com/'
+  //     }
+  //   ]);
+  // }
 }));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ controller/wechat/reply.js
@@ -0,0 +1,64 @@
+// 微信关键词回复模块
+module.exports = function(message, req) {
+  switch (message.MsgType) {
+    // 关注和取注公众号
+    // { ToUserName: 'gh_471f0c38b32e',
+    // FromUserName: 'ogRt9s7uVTAh8CumtbfKLQqpIKaM',
+    // CreateTime: '1487209890',
+    // MsgType: 'event',
+    // Event: 'unsubscribe',/'subscribe'
+    // EventKey: '' }
+    case 'event':
+      break;
+      // 文本类型
+      // { ToUserName: 'gh_471f0c38b32e',
+      // FromUserName: 'ogRt9s7uVTAh8CumtbfKLQqpIKaM',
+      // CreateTime: '1487208686',
+      // MsgType: 'text',
+      // Content: 'hehe',
+      // MsgId: '6387512669246748009' }
+    case 'txt':
+    	switch(message.Content){
+    		case 'h':
+    			req.reply({
+    				content:'成功',
+    				type:'text'
+    			});
+    		break;
+    	}
+      break;
+      // 发送位置
+      // { ToUserName: 'gh_471f0c38b32e',
+      // FromUserName: 'ogRt9s7uVTAh8CumtbfKLQqpIKaM',
+      // CreateTime: '1487208711',
+      // MsgType: 'location',
+      // Location_X: '29.980747',
+      // Location_Y: '120.616739',
+      // Scale: '15',
+      // Label: '绍兴市越城区舜江路口公交站东南(门前江公寓西南)',
+      // MsgId: '6387512776620930428' }
+    case 'location':
+      break;
+      // 发送音频(需要根据mediaid 去微信服务器获取资源，然后上传到存储服务器或自己本地)
+      //{ ToUserName: 'gh_471f0c38b32e',
+      // FromUserName: 'ogRt9s7uVTAh8CumtbfKLQqpIKaM',
+      // CreateTime: '1487210222',
+      // MsgType: 'voice',
+      // MediaId: 'FGZ1ZHUAN9JYu32GQ-sEASI-fMWb7SI78ElX5jGQaR1ERlN_QEgYJ8d7k4ks1kIv',
+      // Format: 'amr',
+      // MsgId: '6387519266316515292',
+      // Recognition: '' }
+    case 'voice':
+      break;
+      // 发送图片(需要根据mediaid 去微信服务器获取资源，然后上传到存储服务器或自己本地)
+      //     { ToUserName: 'gh_471f0c38b32e',
+      // FromUserName: 'ogRt9s7uVTAh8CumtbfKLQqpIKaM',
+      // CreateTime: '1487210279',
+      // MsgType: 'image',
+      // PicUrl: 'http://mmbiz.qpic.cn/mmbiz_jpg/1Rtjcu21hfypf4HQaVjd3SiacicoeaQTf6bWPdOsibs9VZiaesxiaLFeSbIMUMziajOF1P7iceDbAvbEEFJXZnsErD9Lg/0',
+      // MsgId: '6387519511129651175',
+      // MediaId: 'jdEG9VI5rUQNE4JDWPTTiISZl4yJmOWhqJ4Pfpf2EaEMH25sf4-GlUoxKseMfhYd' }
+    case 'image':
+      break;
+  }
+}
