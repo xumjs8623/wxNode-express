ff61c8ff0bc24b703fb3266ab72c7be7617999c9
mj <xumj8955@126.com>
2017-02-16 15:48:04 +0800

    12313

--- common/dbMysql.js
+++ common/dbMysql.js
@@ -1,11 +1,11 @@
 var dbConfig = require('../config/db.js');
-module.exports = function (sqlStr, params) {
+module.exports = function(sqlStr, params) {
   var mysql = require('mysql');
   var connection = mysql.createConnection(dbConfig.mysql);
 
   connection.connect();
-
-    var query = connection.query(sqlStr, function (err, rows, fields) {
+  if (params == '') {
+    var query = connection.query(sqlStr, function(err, rows, fields) {
       if (err) throw err;
       console.log(rows);
       // var data=[];
@@ -14,8 +14,22 @@ module.exports = function (sqlStr, params) {
       // }
       // console.log(data);
     });
+  } else {
+    var query = connection.query(sqlStr, params
+      function(err, rows, fields) {
+        if (err) throw err;
+        console.log(rows);
+        // var data=[];
+        // for(var i=0;i<rows.lenght;i++){
+        //   data.push(rows[i].RowDataPacket);
+        // }
+        // console.log(data);
+      });
+  }
+
 
 
   console.log(query.sql);
   connection.end();
+
 };
