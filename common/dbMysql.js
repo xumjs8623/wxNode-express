59766b268fa175060b576bbf21deba5f9f29966d
徐民军 <mj@xuminjundeMacBook-Pro.local>
2017-02-16 14:38:11 +0800

    1231

--- common/dbMysql.js
+++ common/dbMysql.js
@@ -1,12 +1,18 @@
 var dbConfig = require('../config/db.js');
-module.exports = function(sqlStr,params) {
+module.exports = function (sqlStr, params) {
   var mysql = require('mysql');
   var connection = mysql.createConnection(dbConfig.mysql);
 
   connection.connect();
 
-  var query=connection.query(sqlStr, params, function(err, rows, fields) {
+  var query = connection.query(sqlStr, params, function (err, rows, fields) {
     if (err) throw err;
+    console.log('err');
+    console.log(err);
+    console.log('rows');
+    console.log(rows);
+    console.log('fields');
+    console.log(fields);
     // console.log('The solution is: ', rows[0].solution);
   });
   console.log(query.sql)
